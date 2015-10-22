d7e7ce5ea31267c62bac8e17b796df7bf0672236
Jan Biasi <biasijan@gmail.com>
2015-10-22 21:17:28 +0200

    Adding photon and demo content

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var app = require('app');
 var pkg = require('./package.json');
 var BrowserWindow = require('browser-window');
-require('crash-reporter').start();
-
 var mainWindow = null;
+
+require('crash-reporter').start();
 
 app.on('window-all-closed', function() {
     if (process.platform != 'darwin') {
@@ -12,11 +12,7 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
-    mainWindow = new BrowserWindow({
-        width: pkg.window.width,
-        height: pkg.window.height
-    });
-
+    mainWindow = new BrowserWindow(pkg.window);
     mainWindow.loadUrl('file://' + __dirname + '/index.html');
     mainWindow.openDevTools();
 
